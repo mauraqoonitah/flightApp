4d69b60edfef29c4c7502a7eaa6bb1df355c21fd
mauraqp <mauraqoonitah@gmail.com>
2021-04-04 23:59:54 +0700

    update layout

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $data = file_get_contents("./data/data.json");
 $data_arr = json_decode($data, true);
 // var_dump($data_arr[1]); // print array
+
 ?>
 
 <!DOCTYPE html>
@@ -17,12 +18,11 @@ $data_arr = json_decode($data, true);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <!-- Favicon-->
-    <link href="./gambar/icon.png" rel="shortcut icon" type="image/ico" />
+    <link href="./gambar/icon.png" rel="shortcut icon" type="image/icon" />
     <!-- my css -->
     <link rel="stylesheet" href="./library/css/style.css">
-    <link rel="stylesheet" href="./library/css/all.css">
-    <!-- icon bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <!-- fontawesome icon -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 
     <title>Flight</title>
 </head>
@@ -38,8 +38,9 @@ $data_arr = json_decode($data, true);
 
         </div>
     </nav>
-    <main>
 
+    <main>
+        <!-- =============== SECTION PENDAFTARAN RUTE PENERBANGAN ================= -->
         <section class="pendaftaran">
             <div class="container-sm">
                 <h2 class="text-center p-5">Pendaftaran Rute Penerbangan</h2>
@@ -75,68 +76,85 @@ $data_arr = json_decode($data, true);
                         </div>
                     </div>
 
-                    <button type="submit" class="btn btn-success mt-4 mb-4">
-                        <i class="bi bi-search"></i>
-                        Tambah</button>
+                    <button type="submit" class="btn btn-success my-4 fw-bold">
+                        TAMBAH</button>
 
                 </form>
             </div>
         </section>
+
+        <!-- =============== SECTION DAFTAR RUTE TERSEDIA ================= -->
         <section class="hasil-rute" id="hasil-rute">
             <div class="container-sm mt-5 mb-5 pb-5">
                 <h2 class="text-center p-5">Daftar Rute Tersedia</h2>
                 <?php foreach ($data_arr as $data) : ?>
-                    <form class="row g-3 col-md-10 mx-auto box shadow mt-4">
-                        <div class="col-md-4">
+                    <form class="row g-3 col-lg-6 mx-auto box shadow mt-4">
+                        <div class="col-md-6 px-3">
+
                             <i class="fas fa-circle mini"></i>
                             <i class="fas fa-circle mini"></i>
                             <i class="fas fa-circle mini"></i>
                             <i class="fas fa-plane"></i>
                             <br>
+
+                            <!-- =============== MASKAPAI ================= -->
                             <p class="form-content"> <?= $data[0]; ?></p>
+
+                            <div class="col-lg-12">
+                                <div class="line_style__content">
+
+                                    <div class="line_style__item">
+                                        <span class="line_style__round"></span>
+                                        <span class="line_style__line"></span>
+                                    </div>
+
+                                    <!-- =============== ASAL BANDARA ================= -->
+                                    <div>
+                                        <p class="fw-bold"> <?= $data[1]; ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="line_style__content">
+
+                                    <div class="line_style__item">
+                                        <span class="line_style__rounder"></span>
+                                    </div>
+                                    <!-- =============== TUJUAN BANDARA ================= -->
+
+                                    <div>
+                                        <p class="fw-bold"> <?= $data[2]; ?></p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
-                        <div class="col-md-4">
-                            <div class="col-md-12">
-                                <i class="fas fa-circle"></i>
-                                <p class="fw-bold"> <?= $data[1]; ?></p>
+                        <div class="col-md-6 px-3">
 
-                            </div>
-                            <i class="fas fa-circle dot"></i>
-                            <br>
-                            <i class="fas fa-circle dot"></i>
-                            <!-- <i class="fas fa-arrow-down"></i> -->
-                            <br>
-                            <i class="fas fa-circle dot"></i>
-
-                            <div class="col-md-12">
-                                <br>
-                                <i class="fas fa-circle"></i>
-                                <p class="fw-bold"> <?= $data[2]; ?></p>
-                                </g>
-                            </div>
-                        </div>
-
-                        <div class="col-md-4 text-md-end">
-                            <div class="col-md-12">
-                                <p class="form-label fs-6 fw-bold">Harga Tiket: </p>
-                                <p class="fw-light"> <?= $data[3]; ?></p>
-                                <hr>
-                            </div>
-                            <div class="col-md-12">
-                                <p class="form-label fs-6 fw-bold">Pajak: </p>
-                                <p class="fw-light"> <?= $data[4]; ?></p>
-                                <hr>
+                            <!-- =============== HARGA TIKET ================= -->
+                            <div class="col-md-12 px-4 py-2">
+                                <p class="form-label fs-6 fw-bold d-inline pb-5">Harga Tiket : </p>
+                                <p class="fw-light d-inline"> <?= $data[3]; ?></p>
+                                <hr class="mt-2 mb-2">
                             </div>
 
-                            <div class="col-md-12">
-                                <p class="form-label fs-6 fw-bold">Total: </p>
-                                <p class="total"> Rp<?= $data[3]; ?></p>
+                            <!-- =============== PAJAK ================= -->
+                            <div class="col-md-12 px-4 py-2">
+                                <p class="form-label fs-6 fw-bold d-inline">Pajak : </p>
+                                <p class="fw-light d-inline"> <?= $data[4]; ?></p>
+                                <hr class="mt-2 mb-2">
+                            </div>
+
+                            <!-- =============== TOTAL HARGA ================= -->
+                            <div class="col-md-12 px-4 py-2">
+                                <p class="total-label fs-6 fw-bold d-inline">Total: </p>
+                                <p class="total d-inline"> Rp <?= $data[3]; ?></p>
                             </div>
                         </div>
                     </form>
                 <?php endforeach; ?>
             </div>
+
         </section>
     </main>
 
